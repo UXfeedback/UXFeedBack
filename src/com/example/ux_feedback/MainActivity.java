873ea941d3b21c508622941fc73414df85da9c6c
soren2013 <sorenmjensen@gmail.com>
2013-10-21 14:14:38 +0200

    Basal survey 1 funktionalitet tilføjet. Uploader for at teste om der stadigvæk er github problemer :)

--- src/com/example/ux_feedback/MainActivity.java
+++ src/com/example/ux_feedback/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ux_feedback;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,16 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void btn1_Clicked (View v){
+		Intent intent = new Intent(this, SurveyPopup_Method1.class);
+		startActivity(intent);
+	}
+	
+	public void btn2_Clicked (View v){
+		Intent intent = new Intent(this, SurveyPopup_Method2.class);
+		startActivity(intent);
 	}
 
 }
